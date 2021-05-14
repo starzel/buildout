fc5ef4d20d1214d05a1701608fd85f88de4ee717
Philip Bauer <bauer@starzel.de>
2021-05-14 09:45:24 +0200

    workon ci

--- bootstrap_ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo -e "[buildout]\nlogin = admin\npassword = admin" > secret.cfg
-ln -fs local_test.cfg local.cfg
-./bin/pip install -r requirements.txt
-./bin/develop up -f || true
-./bin/buildout -t 3 || ./bin/buildout -v
